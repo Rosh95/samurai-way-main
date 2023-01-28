08347279b0f9362b8cfeec1ca995a7e9d696e5e0
Rosh1995 <azi.rovshan@gmail.com>
2023-01-28 18:01:04 +0300

    installing dependencies yarn.lock

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <a href='#'>Home </a>
+            <a href='#'>News </a>
+            <a href='#'>Messages from friends </a>
+        </div>
+    )
+
+}
+
+export default Header;
---
+++ src/components/Technologies/Technologies.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Technologies() {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>js</li>
+                <li>html</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
+
+export  default  Technologies;
